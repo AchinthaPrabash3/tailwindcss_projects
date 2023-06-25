3257cdc6732f2719c179e964938dafccfd356783
AchinthaPrabash3 <achinthaprabash3@gmail.com>
2023-06-25 13:36:39 +0530

    changed few things

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "restuent": "url('./public/imges/img1.jpg')",
+      },
       boxShadow: {
         "3xl": "0 -5px 50px -15px rgba(0, 0, 0, 0.3)",
       },
